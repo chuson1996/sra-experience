edab94b3a361411b57a0ccc9046c55737cb5dcea
Son Chu <chuson1996@gmail.com>
2022-06-02 17:47:26 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'url'
+import path from 'path';
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'ImInLOL',
+      fileName: (format) => `imin.lol.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        // globals: {
+        //   vue: 'Vue'
+        // }
+      }
+    }
+  }
+})
